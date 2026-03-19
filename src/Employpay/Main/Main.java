2cb6a8cd94f67e78bf75a5652df493dfe42b0b9f
Sumeet <sumeetp725@gmail.com>
2026-03-19 17:41:08 +0530

    first commit

--- src/Employpay/Main/Main.java
+++ src/Employpay/Main/Main.java
@@ -1,7 +1,13 @@
 package Employpay.Main;
 
 public class Main {
-    public Static Void main(String[] args){
+    public static void main(String[] args){
+
+        Employee[] employees={new FulltimeEmployee("raj", 21241675,41000,25000),
+                                new PartTimeEmployee("bob",21241676,2500,250)};
+        for(Employee employee: employees){
+            System.out.println(employee.displayinfo());
+        }
 
     }
 }
--- src/Twopointers.java
+++
@@ -1,8 +0,0 @@
-public class Twopointers {
-    public void reverse(){
-
-    }
-}
-public static void main(String[] args){
-
-}
